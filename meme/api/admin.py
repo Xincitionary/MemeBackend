98f9fd275d305b7be297a42f5256d0d9b15affb5
Eric He <zh975@nyu.edu>
2022-03-28 23:24:40 -0700

    bugged code, needs fix

--- meme/api/admin.py
+++ meme/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import UserLogins
+from .models import *
 
 # Register your models here.
-admin.site.register(UserLogins)
+admin.site.register(UserLogin)
+admin.site.register(Topic)
+admin.site.register(UserPost)
+admin.site.register(Story)
+admin.site.register(Feed)
--- meme/api/models.py
+++ meme/api/models.py
@@ -1,19 +1,75 @@
 from django.db import models
-from django.utils import timezone
 
 #on_delete: https://stackoverflow.com/questions/38388423/what-does-on-delete-do-on-django-models
 #meta: https://docs.djangoproject.com/en/4.0/ref/models/options/
 
 # Create your models here.
-class UserLogins(models.Model):
-    # userID = models.AutoField(primary_key=True, unique = True)
+class UserLogin(models.Model):
+    # UserLogin_id = models.AutoField(primary_key=True, unique = True)
     username = models.CharField(max_length=36)
     password = models.CharField(max_length=36)
     create_time = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table = 'UserLogin'
-
+        managed = False
     def __str__(self):
         return f"{self.id}: {self.username}"
 
 
+class Topic(models.Model):
+    #topic id
+    creatorID =models.ForeignKey(UserLogin, models.CASCADE)
+    num_followers = models.IntegerField()
+    num_feeds = models.IntegerField()
+    num_stories = models.IntegerField()
+    trending = models.BooleanField()
+    topicName = models.CharField(max_length = 45)
+    abstract = models.CharField(max_length=200)
+    class Meta:
+        db_table = 'Topic'
+        managed = False
+    def __str__(self):
+        ret = "the topic title is " + self.topicName
+        return ret
+
+
+class UserPost(models.Model):
+    visibility = models.CharField(max_length=36)
+    anonymous = models.BooleanField()
+    view_count = models.IntegerField()
+    topicID = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    userID =  models.ForeignKey(UserLogin, on_delete = models.CASCADE)
+    parentID = models.ForeignKey('self',on_delete = models.CASCADE)
+    is_story = models.BooleanField()
+    class Meta:
+        db_table = 'Posts'
+        managed = False
+    def __str__(self):
+        return f"{self.id}: {self.username}"
+
+
+#child of the topic class 
+class Story(models.Model):
+    storyID = models.OneToOneField(
+        UserPost,
+        on_delete= models.CASCADE,
+        primary_key= True,
+    )
+    content = models.CharField(max_length=1000)
+    class Meta:
+        db_table = 'Stories'
+        managed = False
+
+
+#child of the topic class 
+class Feed(models.Model):
+    storyID = models.OneToOneField(
+        UserPost,
+        on_delete= models.CASCADE,
+        primary_key= True,
+    )
+    content = models.CharField(max_length=200)
+    emoji = models.IntegerField()
+    class Meta:
+        db_table = 'Feeds'
+        managed = False
